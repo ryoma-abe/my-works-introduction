45f3cf8f1c3e8659aa3346886f12ae01bc0e11c3
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-15 08:20:09 +0900

    データベースから「Work」データを取得し、存在しない場合は404エラーページを表示する機能を追加しました。また、タイトルと説明を動的に表示するように変更しました。

--- src/app/works/new/page.tsx
+++ src/app/works/new/page.tsx
@@ -1,18 +1,27 @@
-// import prisma from "@/lib/prisma";
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
 
 export default async function Work({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
+  const { id } = await params;
+  const work = await prisma.work.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!work) {
+    notFound();
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <article className="max-w-2xl space-y-4 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-4xl font-bold mb-8 text-[#333333]">
-          My first post
-        </h1>
-        <p className="text-gray-600 text-center">by Anonymous</p>
-        <div className="prose prose-gray mt-8">No content available.</div>
+        <h1 className="text-4xl font-bold mb-8 text-[#333333]">{work.title}</h1>
+        <div className="prose prose-gray mt-8">
+          {work.description || "コンテンツはありません"}
+        </div>
       </article>
     </div>
   );
