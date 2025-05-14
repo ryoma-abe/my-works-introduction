9427da1e6e5faabb0e1a3fa13538fc1945ca5c9d
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-15 07:23:19 +0900

    Prismaを使用してデータベースから「Work」データを取得し、トップページに表示する機能を追加しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>トップページ</div>;
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+  const works = await prisma.work.findMany();
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {works.map((work) => (
+          <li key={work.id} className="mb-2">
+            {work.title}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
