f4bdaa8242766eeb777e1f2062d055a7cd155063
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-16 08:10:59 +0900

    ホームページのレイアウトを更新し、背景色を黒に変更しました。また、子要素をメインコンテンツとしてラップし、最小高さとパディングを追加しました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-        {children}
+        <main className="min-h-screen py-20">{children}</main>
       </body>
     </html>
   );
