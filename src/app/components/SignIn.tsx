f559c8dcfc1f3d531662991ad86b7abceaf7507f
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-17 08:11:29 +0900

    SignInコンポーネントのボタンテキストを「GitHubでサインイン」に変更し、サインイン時のコールバックURLを「/dashboard」に設定しました。

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -3,5 +3,9 @@
 import { signIn } from "next-auth/react";
 
 export default function SignIn() {
-  return <button onClick={() => signIn("github")}>ログイン</button>;
+  return (
+    <button onClick={() => signIn("github", { callbackUrl: "/dashboard" })}>
+      GitHubでサインイン
+    </button>
+  );
 }
